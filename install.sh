1d345cce5fca39f6cf32a1cf11b61385bdb01efa
Martin <martin@oz1tmm.dk>
2024-04-05 16:34:00 +0200

    Revert "installing all vimrc, zshrc and settings.json for vscode"
    This reverts commit 6afab71c82c0828840c12d4b9c1056855f3c83a2.

--- install.sh
+++ install.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 set -e
-result=${PWD}          # to assign to a variable
 
-FILES=(
-	"~/.config/Code/User/settings.json" 
-	"~/.vimrc" 
-	"~/.zshrc")
-
-for file in ${FILES[*]};
-do
-if [ -f $file ]; then
-	#echo $file, $file"_dotfiles"
-	mv $file $file"_beforedotfiles"
-fi
-done
-
-ln -s ${PWD}/vscode/settings.json ~/.config/Code/User/settings.json
-ln -s ${PWD}/.zshrc ~/.zshrc
-ln -s ${PWD}/.vimrc ~/.vimrc
+ln -s ~/Documents/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
 
