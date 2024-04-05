6afab71c82c0828840c12d4b9c1056855f3c83a2
mml <mml@mml-desktop.local.oz1tmm.dk>
2024-04-05 16:31:32 +0200

    installing all vimrc, zshrc and settings.json for vscode

--- install.sh
+++ install.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 set -e
+result=${PWD}          # to assign to a variable
 
-ln -s ~/Documents/dotfiles/vscode/settings.json ~/.config/Code/User/settings.json
+FILES=(
+	"~/.config/Code/User/settings.json" 
+	"~/.vimrc" 
+	"~/.zshrc")
+
+for file in ${FILES[*]};
+do
+if [ -f $file ]; then
+	#echo $file, $file"_dotfiles"
+	mv $file $file"_beforedotfiles"
+fi
+done
+
+ln -s ${PWD}/vscode/settings.json ~/.config/Code/User/settings.json
+ln -s ${PWD}/.zshrc ~/.zshrc
+ln -s ${PWD}/.vimrc ~/.vimrc
 
